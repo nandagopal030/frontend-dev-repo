3b7b7ee393b046acb5e3752bdc2cba47c87ba8d7
Your Name <nandagopal030@gmail.com>
2025-12-10 10:31:34 +0530

    Values and variables

---
+++ projects/javascript-notes/complete-javascript-course-master/01-Fundamentals-Part-1/starter/Readme.txt
@@ -0,0 +1,6 @@
+https://assignments-c03.jonas.io/instructions.html
+
+Values and Variables﻿
+Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+Log their values to the console.
--- projects/javascript-notes/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ projects/javascript-notes/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,21 +1,32 @@
 console.log("I love Javascript");
 
 // A value is basically a smallest unit of inforamtion we have in javascript (example)
-let age = 10; //let is a datatype and age is the variable and 10 is the value
+let age = 10; //let is a datatype and age is the variable and 10 is the value (It is a declared statement)
 
-// A variable would not start with a number / symbol it cause a unexpected syntax error  ( _   and   $ ) sign are oly allowed
+// A variable would not start with a number / symbol it cause a unexpected syntax error  ( _   and   $ ) sign are only allowed
 _name = "nanda";
 $name = "gopal";
-// let 3name = "nanda";  This will cause an error and the error will be displayed in console
-//should not start a variable with uppercase letter
+// let 3name = "nanda";  This will cause an error and the error will be displayed in console inside the browser
+//should not start a variable with uppercase letter instead use the camel case (firstName)
 
-let PI = 3.14;
+let PI = 3.14;   //convention
 console.log(PI);
 
-//correct camel case approach
+//correct camel case approach    It is more descriptive variable assigning approach
 let myCurrentJob = "Programmer";
 let myOldJob = "Software";
 
 //non descriptive approach
 let job1 = "teacher";
 let job2 = "student";
+
+let _function = "reception"
+let $function = "reception"
+
+//A variable should not be used as a reserved javascript keyword example
+// let new = "name";   here new is a reserve js keyword (It shows a unexpected tocken error)
+
+//Another one reserved keyword and illegal to use example
+let name = "banda"; //name is reserved keyword inseted we can use firstName / lastName in javascript
+
+//A Variable which is all in Uppercase are reserved for constants
