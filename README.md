dce2602838e9d36451ca7b43b7090080abcc2445
Your Name <nandagopal030@gmail.com>
2025-12-10 09:51:28 +0530

    starter

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 This repository is a whole track for my development journey while learning REACT + JS + TS + HTML + CSS where all the notes and pocs and projects/mini projects  will be pushed in this repository (a bunch of Learning Stuff)
 Excel sheet of tracking my journey  :: https://docs.google.com/spreadsheets/d/1Lf5BvlQUjUW-rNbvLOSbW-GaLLFuuat67152aA2oOTw/edit?gid=1885345083#gid=1885345083
+
+
+Javascript Fundamentals part - 1 and part - 2 (assesments);
+https://assignments-c03.jonas.io/instructions.html
---
+++ projects/javascript-notes/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+console.log("I love Javascript");
+
+// A value is basically a smallest unit of inforamtion we have in javascript (example)
+let age = 10; //let is a datatype and age is the variable and 10 is the value
+
+// A variable would not start with a number / symbol it cause a unexpected syntax error  ( _   and   $ ) sign are oly allowed
+_name = "nanda";
+$name = "gopal";
+// let 3name = "nanda";  This will cause an error and the error will be displayed in console
+//should not start a variable with uppercase letter
+
+let PI = 3.14;
+console.log(PI);
+
+//correct camel case approach
+let myCurrentJob = "Programmer";
+let myOldJob = "Software";
+
+//non descriptive approach
+let job1 = "teacher";
+let job2 = "student";
