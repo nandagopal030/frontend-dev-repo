f1e530f2da9c7d002ec2171084ccd4435043f998
Your Name <nandagopal030@gmail.com>
2025-12-16 14:19:32 +0530

    strict mode concept

--- projects/javascript-notes/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ projects/javascript-notes/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-/* 
+
 'use strict';      // This is called the javascript strict mode when we use this on the top page of our javascript application this would catch the unfound errors which are being written in the code
 const hasDriversLicense = false;
 const passTest = true;
@@ -7,4 +7,4 @@ if (hasDriversLicense) console.log("You can drive your car : ");
 console.log("Hello");
 
 const interface = 'Audio';    //errror :: Uncaught SyntaxError: Unexpected strict mode reserved word (at script.js:8:7)
-*/
+const private = "i am a private"; //error :: The ( private ) is a reserved keyword and ( if ) ( while ) ( for ) many things which was not mentioned here is a reserved keyword so if we use the reserved keyword with mentioning the strict mode it throws an error 
