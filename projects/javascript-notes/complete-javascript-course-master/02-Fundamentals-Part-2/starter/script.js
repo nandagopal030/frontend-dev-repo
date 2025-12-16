db1661c1e89a639a86c156fcf707b087041557f6
Your Name <nandagopal030@gmail.com>
2025-12-16 12:48:36 +0530

    jjj

--- projects/javascript-notes/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ projects/javascript-notes/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,10 @@
-'use strict';   // strict mode avoid accidentail errors (without strict mode js will simply fail silently)
+/* 
+'use strict';      // This is called the javascript strict mode when we use this on the top page of our javascript application this would catch the unfound errors which are being written in the code
+const hasDriversLicense = false;
+const passTest = true;
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("You can drive your car : ");
+console.log("Hello");
+
+const interface = 'Audio';    //errror :: Uncaught SyntaxError: Unexpected strict mode reserved word (at script.js:8:7)
+*/
