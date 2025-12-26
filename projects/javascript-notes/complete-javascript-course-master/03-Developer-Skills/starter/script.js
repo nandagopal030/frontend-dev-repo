264eaa6a38892fac2c13818d031e6791e5af28d5
nandy <nandagopal030@gmail.com>
2025-12-26 17:46:09 +0530

    skipped section

--- projects/javascript-notes/complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ projects/javascript-notes/complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,6 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 20;
+
+//skipped this section  because i fount this is not much relevant and not much needed for me
