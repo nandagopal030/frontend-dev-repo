2ef2762a64f4f253b2adbeb509332e15ee45a746
nandy <nandagopal030@gmail.com>
2026-01-09 05:31:09 +0530

    destructuring arrays

--- projects/javascript-notes/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ projects/javascript-notes/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -30,6 +30,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starter, mainDish) {
+    return [this.starterMenu[starter], this.mainMenu[mainDish]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -45,3 +49,36 @@ const restaurant = {
     },
   },
 };
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Array Destructuring
+const arr = [2, 3, 4];
+const [x, y, z] = arr;
+
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//swap manually
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+//switching variables using destructuring
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+const [s, m] = restaurant.order(1, 1);
+console.log(s, m);
+
+// Destructuring in an nested array
+const nested = [1, 2, [3, 4]];
+const [a, , [b, c]] = nested;
+console.log(a, b, c);
+
+const [p = 1, q = 1, r = 1] = [1, 2];
+console.log(p, q, r);
