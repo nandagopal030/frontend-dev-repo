bd04c729e55076cd1db4cc0fbe09fd99d71acf2d
nkannan4 <nandagopal.kannan@valeo.com>
2026-02-04 09:28:53 +0530

    mmm

--- projects/javascript-notes/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ projects/javascript-notes/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -81,18 +81,24 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const date = new Date(acc.movementsDates[i]);
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const displayDate = `${day}/${month}/${year} `;
 
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1
       } ${type}</div>
+        <div class="movements__date">${displayDate}€</div>
         <div class="movements__value">${mov}€</div>
       </div>
     `;
@@ -141,7 +147,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -153,6 +159,23 @@ const updateUI = function (acc) {
 ///////////////////////////////////////
 // Event handlers
 let currentAccount;
+
+// FAKE ALWAYS LOGGEDIN
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
+
+const now = new Date();
+const day = now.getDate();
+const month = now.getMonth() + 1;
+const hours = now.getHours();
+const min = now.getMinutes();
+const sec = now.getSeconds();
+const year = now.getFullYear();
+labelDate.textContent = `${day}/${month}/${year}  ${hours}: ${min}: ${sec}`;
+
+
+
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -169,6 +192,14 @@ btnLogin.addEventListener('click', function (e) {
       }`;
     containerApp.style.opacity = 100;
 
+    const now = new Date();
+    const day = now.getDate();
+    const month = now.getMonth() + 1;
+    const hours = `${now.getHours()}`.padStart(2,0);
+    const min = `${now.getMinutes()}`.padStart(2,0);
+    const sec = now.getSeconds();
+    const year = now.getFullYear();
+    labelDate.textContent = `${day}/${month}/${year}  ${hours}: ${min}: ${sec}`;
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
@@ -196,6 +227,10 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    //Add transfer Date 
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -209,6 +244,8 @@ btnLoan.addEventListener('click', function (e) {
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
     currentAccount.movements.push(amount);
+    //Add transfer Date 
+    currentAccount.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -258,7 +295,7 @@ console.log(0.1 + 0.2 == 0.3);
 
 //conversion
 console.log(Number('22'));
-console.log(+'22'); // The + plus Symbol makes the number cohertion 
+console.log(+'22'); // The + plus Symbol makes the number cohertion
 
 console.log('-------------------parseInt---------------------------------------');
 //Parsing
@@ -414,6 +451,7 @@ console.log(new Date (2033, 11, 32, 42, 12, 33));
 console.log(new Date(0));
 console.log(new Date(2037 * 24 * 60 * 60 * 20));
 */
+/*
 //working with Dates
 const future = new Date(2037, 10, 19, 15, 24);
 console.log(future);
@@ -424,7 +462,7 @@ console.log(future.getDay());
 console.log(future.getHours());
 console.log(future.getMinutes());
 console.log(future.getSeconds());
-console.log(future.toISOString()); // format of international standard 
+console.log(future.toISOString()); // format of international standard
 console.log(future.getTime());
 console.log(new Date(2142237240000));
 
@@ -432,3 +470,5 @@ console.log(Date.now());
 
 future.setFullYear(2992);
 console.log(future);
+*/
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
