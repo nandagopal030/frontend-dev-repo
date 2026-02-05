d6fcc1dd37d94a7d064adc2b503974162e4c90c9
nkannan4 <nandagopal.kannan@valeo.com>
2026-02-05 14:40:16 +0530

    up to promisses

--- projects/javascript-notes/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ projects/javascript-notes/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
 
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
@@ -9,4 +7,189 @@ const countriesContainer = document.querySelector('.countries');
 // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
+// API
+//https://countries-api-836d.onrender.com/countries/
+
+
+
 ///////////////////////////////////////
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+
+// const getCountryData = function (country) {
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//     request.send();
+//     // console.log(request.responseText);
+//     request.addEventListener('load', function () {
+//         const [data] = JSON.parse(this.responseText);
+//         console.log(data);
+//         const html = `
+//       <article class="country">
+//           <img class="country__img" src="${data.flag}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//           </div>
+//         </article>
+//     `
+//         countriesContainer.insertAdjacentHTML('beforeend', html);
+//         countriesContainer.style.opacity = 1;
+//     });
+// };
+// getCountryData('portugal');
+// getCountryData('usa');
+// getCountryData('germany');
+
+//Callback hell (when we have a lot of nested callback function for AjAX Calls)
+const renderData = function (data, className = '') {
+    const html = `
+    <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+    </article>
+    `
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+}
+
+
+/*
+const getNeighbourCountry = function (country) {
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.com/v2/name/${country}`);
+request.send();
+// console.log(request.responseText);
+request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderData(data);
+
+    // Neighbour country
+    const neighbour = data.borders?.[0];
+    if (!neighbour) return;
+    console.log(neighbour);
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+        const data2 = JSON.parse(request2.responseText);
+        console.log(data2);
+        renderData(data2, 'neighbour');
+    });
+});
+};
+getNeighbourCountry('usa');
+
+setTimeout(() => {
+console.log('1 second passed');
+setTimeout(() => {
+    console.log('2 second passed')
+    setTimeout(() => {
+        console.log('3 second passed')
+        setTimeout(() => {
+            console.log('4 second passed')
+        }, 1000)
+    }, 1000)
+}, 1000)
+}, 1000)
+
+*/
+
+// const dataApi = fetch(' https://restcountries.com/v2/name/portugal');
+// console.log(dataApi)
+
+
+
+// samething using promises
+
+// const getCountryDate = function (country) {
+//     const dataApi = fetch(`https://restcountries.com/v2/name/${country}/`)
+//         .then(function (response) {
+//             console.log(response);
+//             return response.json();
+//         }).then(function (data) {
+//             console.log(data);
+//             renderData(data[0]);
+//         })
+// }
+// getCountryDate('portugal');
+
+
+
+
+const getCountryDate = function (country) {
+    const dataApi = fetch(`https://restcountries.com/v2/name/${country}/`)
+        .then((response) => response.json())
+        .then((data) => renderData(data[0]));
+}
+getCountryDate('portugal');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Lecture
+// Asyncronous programming 
+/*
+Asyncronous programming is all about co-ordinationg the behaviour of the program for the certain amount of time
+setTimeout() -> works in Asyncronous way and
+-> callback function alone does not allows the code asyncronous 
+-> Event Listners alone does not allows the code asyncronous 
+
+// What is AJAX
+Asyncronous  JAvascript And XML 
+XML is a data format which is used to be widely used to transmit data on the web
+but now we use the JSON format
+*/
+
+
