66567544456b0a8d39eab305f7b64a486fe4f391
nkannan4 <nandagopal.kannan@valeo.com>
2026-02-09 15:54:25 +0530

    All promise topics

--- projects/javascript-notes/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ projects/javascript-notes/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -30,7 +30,7 @@ const renderData = function (data, className = '') {
     </article>
     `
     countriesContainer.insertAdjacentHTML('beforeend', html);
-    // countriesContainer.style.opacity = 1;
+    countriesContainer.style.opacity = 1;
 }
 
 
@@ -40,6 +40,13 @@ const renderError = function (errorMsg) {
     // countriesContainer.style.opacity = 1;
 }
 
+const getJSON = function (url, errorMsg = '') {
+    return fetch(url).then((response) => {
+        if (!response.ok)
+            throw new Error(`country not found (${response.status}) ${errorMsg}`);
+        return response.json();
+    })
+}
 
 // const getCountryData = function (country) {
 //     const request = new XMLHttpRequest();
@@ -272,7 +279,7 @@ Promise.reject(new Error('abc')).then(x => console.error(x));
 
 */
 
-
+/*
 
 const getPosition = function () {
     return new Promise(function (resolve, reject) {
@@ -317,6 +324,131 @@ whereAmI(52.508, 13.381);
 
 const btn = document.querySelector('.btn-country');
 btn.addEventListener('click', whereAmI)
+
+
+*/
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Async and await
+/*
+const whereAmI = async function (country) {
+    // fetch(`https://restcountries.com/v2/name/${country}`).then((res) => console.log(res));
+
+    const res = await fetch(`https://restcountries.com/v2/name/${country}`);
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
+    renderData(data[0]);
+}
+whereAmI('portugal');
+console.log('FIrsT');
+*/
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+
+const get3Countries = async function (c1, c2, c3) {
+    try {
+        // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+        // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+        // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+        // console.log([data1.capital, data2.capital, data3.capital]);
+
+
+        const data = await Promise.all([
+            getJSON(`https://restcountries.com/v2/name/${c1}`),
+            getJSON(`https://restcountries.com/v2/name/${c2}`),
+            getJSON(`https://restcountries.com/v2/name/${c3}`)
+        ])
+        console.log(data.map((d) => d[0].capital));
+    } catch (err) {
+        console.error(err);
+    }
+}
+get3Countries('portugal', 'canada', 'tanzania');
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// promise combinators
+
+(async function () {
+    const res = await Promise.race([
+        getJSON(`https://restcountries.com/v2/name/italy`),
+        getJSON(`https://restcountries.com/v2/name/egypt`),
+        getJSON(`https://restcountries.com/v2/name/mexico`)
+    ])
+    console.log(res[0]);
+})();
+
+
+
+const timeOut = function (sec) {
+    return new Promise(function (_, reject) {
+        setTimeout(() => {
+            reject(new Error('Request took so long'));
+        }, sec);
+    })
+}
+
+//promise.race
+Promise.race([
+    getJSON(`https://restcountries.com/v2/name/china`),
+    timeOut(0.5)
+])
+    .then(res => console.log(res[0]))
+    .catch(err => console.log(err));
+
+
+//promise.allSettled
+Promise.allSettled([
+    Promise.resolve('Success'),
+    Promise.reject('Error'),
+    Promise.resolve('Another resolve')
+]).then(res => console.log(res)).catch((err) => console.log(err))
+
+
+
+
+//promise.any - >rejected promisses are removed and first success promise works properly
+Promise.any([
+    Promise.resolve('Success'),
+    Promise.reject('Error'),
+    Promise.resolve('Another resolve')
+]).then(res => console.log(res)).catch((err) => console.log(err))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
